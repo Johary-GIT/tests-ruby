fde221dbeb1cc857c32a244078fe529b4c527e26
Johary-GIT <johariniarivorakoto@gmail.com>
2020-11-23 09:15:40 +0300

    test 3

---
+++ lib/02_calculator.rb
@@ -0,0 +1,41 @@
+def add(a, b)
+	c = a + b
+return (c)
+
+	end 
+
+def subtract(a, b)
+
+	c = a - b
+return (c)
+
+	end 
+
+def sum(tab)
+
+return (tab.sum)
+
+	end 
+
+def multiply(a, b)
+
+	c = a * b 
+return (c)
+
+	end 
+
+def power(n)
+
+	power = n * n  
+return (power)
+
+	end 
+
+def factorial(n)
+
+	if n == 0
+	return (1)
+	else
+	return (n * factorial(n-1))
+	end
+end
