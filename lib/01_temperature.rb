7dda95cf2adfa5a583aad668dbc1a95ec6d58f2b
Johary-GIT <johariniarivorakoto@gmail.com>
2020-11-23 08:51:47 +0300

    test

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,10 +1,9 @@
-# temperature conversion functions : ftoc
+# conversion de temperature : ftoc
 def ftoc(temperature)
   return (temperature - 32) * 5/9
-
 end
 
-# temperature conversion functions : ctof
+# conversion de temperature : ctof
 def ctof(temperature)
   return temperature * (9.0/5.0) + 32
 end
--- spec/00_hello_spec.rb
+++ spec/00_hello_spec.rb
@@ -6,12 +6,15 @@ describe "the hello function" do
   end
 end
 
+
 describe "the greet function" do
   it "says hello to someone" do
     expect(greet("Alice")).to eq("Hello, Alice!")
   end
 
   it "says hello to someone else" do
-    expect(greet("Bob")).to eq("Hello, Bob!")
+    expect(greet("Bob")).to eq("Hello, Bob!"
   end
 end
+
+
